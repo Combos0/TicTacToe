4a968542e6bf161829297ca07dd2bb41dbc50cc1
Combos0 <jcx429.sc2@gmail.com>
2022-09-09 15:42:37 -0400

    Added factory for players, currently rudamentry. Added grid for the game to reside, and added unique id's to each div for each X or O.

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 //////GAME BOARD//////
 const gameBoard = (() => {
-    const board = [];
+    const board = ['X', 'O', '', 'O', 'X', 'O', 'O', 'X', 'O'];
     const display = document.getElementById('game-board');
-    display.innerHTML = board;
+    //display.innerHTML = board;
+
+    const gridCells = (() => {
+        for (i = 0; i < 9; i++) {
+            let newDivs = document.createElement('div');
+            newDivs.setAttribute('id', `grid-div-${i}`)
+            display.appendChild(newDivs);
+        };
+    })();
+
 })();
+
+const playerFactory = (name, color, score) => {
+    const scoreCount = () => console.log(score);
+    return {name, color, score};
+};
+
+const playerX = playerFactory('X', 'red', 0);
+const playerO = playerFactory('O', 'blue', 0);
