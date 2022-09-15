af8ee9b39532e75056fb98575527fe80d986627d
Combos0 <jcx429.sc2@gmail.com>
2022-09-15 15:56:30 -0400

    added event listeners for squares. players cant overwrite already taken squares.

--- main.js
+++ main.js
@@ -9,8 +9,18 @@ const gameBoard = (() => {
             let boardDiv = document.createElement('div');
             boardDiv.setAttribute('id', `grid-div-${i}`);
             display.appendChild(boardDiv);
+            boardDiv.addEventListener('click', () => {
+                if ((playerX.theirTurn === true && playerO.theirTurn === false) && (boardDiv.innerHTML !== 'X' && boardDiv.innerHTML !== 'O')) {
+                    boardDiv.innerHTML = 'X';
+                    playerX.theirTurn = false;
+                    playerO.theirTurn = true;
+                } else if ((playerX.theirTurn === false && playerO.theirTurn === true) && (boardDiv.innerHTML !== 'X' && boardDiv !== 'O')) {
+                    boardDiv.innerHTML = 'O';
+                    playerX.theirTurn = true;
+                    playerO.theirTurn = false;
+                } else {alert('Please select an open square!')};
+            });
             board.push(boardDiv);
-            console.log(board);
         };
     })();
 
