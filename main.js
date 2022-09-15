f6d5148e49bd7fa7416aeaa4d9230fd8066d7562
Combos0 <jcx429.sc2@gmail.com>
2022-09-15 15:25:49 -0400

    Refactored gameboard logic that loads the player cells.

--- main.js
+++ main.js
@@ -1,42 +1,16 @@
 //////GAME BOARD//////
 
 const gameBoard = (() => {
-    const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    const board = [];
     const display = document.getElementById('game-board');
 
-    const gridCells = (() => {
+    const boardSpaces = (() => {
         for (i = 0; i < 9; i++) {
-            let newDivs = document.createElement('div');
-            newDivs.setAttribute('id', `grid-div-${i}`)
-            display.appendChild(newDivs);
-        };
-    })();
-
-    const bridge = (() => {
-        for (i = 0; i < board.length; i++) {
-            let element = document.getElementById(`grid-div-${i}`);
-            element.textContent = board[i];
-        };
-    })();
-
-    const interactiveDivs = (() => {
-        for (i = 0; i < 9; i++) {
-            let gridSpaces = document.getElementById(`grid-div-${i}`);
-            gridSpaces.addEventListener('click', () => {
-                if (gridSpaces.innerHTML === 'X' || gridSpaces.innerHTML === 'O') {
-                    alert('Please pick a different square.');
-                } else if (playerX.theirTurn === true && playerO.theirTurn === false) {
-                    gridSpaces.innerHTML = 'X';
-                    playerX.theirTurn = false;
-                    playerO.theirTurn = true;
-                    console.log(`player 1's turn: ${playerX.theirTurn}, player 2's turn: ${playerO.theirTurn}`);
-                } else if (playerO.theirTurn === true && playerX.theirTurn === false) {
-                    gridSpaces.innerHTML = 'O';
-                    playerO.theirTurn = false;
-                    playerX.theirTurn = true;
-                    console.log(`player 2's turn: ${playerO.theirTurn}, player 1's turn: ${playerX.theirTurn}`);
-                };
-            });
+            let boardDiv = document.createElement('div');
+            boardDiv.setAttribute('id', `grid-div-${i}`);
+            display.appendChild(boardDiv);
+            board.push(boardDiv);
+            console.log(board);
         };
     })();
 
